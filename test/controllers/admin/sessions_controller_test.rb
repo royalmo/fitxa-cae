620de5ba44b529c9f23d7dd38ec36d21116705e5
Eric Roy <eric@ericroy.net>
2026-08-09 13:37:18 +0200

    Clean a bit translations

--- test/controllers/admin/sessions_controller_test.rb
+++ test/controllers/admin/sessions_controller_test.rb
@@ -20,7 +20,7 @@ class Admin::SessionsControllerTest < ActionDispatch::IntegrationTest
     assert_select ".admin-auth-footer a[href='https://cae.cat/avisos-legals/'][target='_blank'][rel='noopener']", text: "Avís legal"
     assert_select ".admin-auth-footer a.auth-footer-access[href='/']", text: "Accés de personal"
     assert_select ".admin-auth-footer a[href='/'][target]", 0
-    assert_match(/v#{Regexp.escape(Rails.configuration.x.app_version)}\s+·\s+Desenvolupament/, response.body)
+    assert_match(/v#{Regexp.escape(Rails.configuration.x.app_version)}\s+·\s+Desenvolupat\sper/, response.body)
     assert_select "form[action='#{admin_login_path}']"
     assert_select "body[data-controller~='admin-theme'][data-controller~='submit-feedback']"
     assert_select "input[type='submit'][data-submitting-label='Entrant...']"
