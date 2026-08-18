db137b3ca3025e8557808bc034b1e0c33ec0d6f0
Eric Roy <eric@ericroy.net>
2026-08-18 22:10:22 +0200

    Fix CI

--- test/mailers/error_report_mailer_test.rb
+++ test/mailers/error_report_mailer_test.rb
@@ -2,14 +2,19 @@ require "test_helper"
 
 class ErrorReportMailerTest < ActionMailer::TestCase
   test "report emails the configured error recipient" do
+    original = ENV["ERROR_NOTIFICATION_SENDER"]
+    ENV["ERROR_NOTIFICATION_SENDER"] = "random-shit@example.com"
+
     mail = ErrorReportMailer.report(sample_error_report)
 
     assert_equal "[FitxaCAE Test ERROR] StandardError: Cannot send login code", mail.subject
     assert_equal [ "eric@ericroy.net" ], mail.to
-    assert_equal [ "errors@#{Rails.configuration.x.app_slug}.invalid" ], mail.from
+    assert_equal [ "random-shit@example.com" ], mail.from
     assert_equal [ "rrhh@cae.cat" ], mail.reply_to
     assert_match "Cannot send login code", mail.body.encoded
     assert_match "delivery_method", mail.body.encoded
+  ensure
+    ENV["ERROR_NOTIFICATION_SENDER"] = original
   end
 
   private
